6fb2afb5b4142338307528b707aeac5f3980e449
Graeme Bell <bell.graeme@gmail.com>
2017-04-11 08:27:08 +0100

    cats

--- public/app.js
+++ public/app.js
@@ -2,37 +2,63 @@ var cats = [
   {
     name: 'boba',
     favouriteFood: 'Sock fluff',
-    image: 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg'
-    imageSize: '500'
+    image: 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg',
+    imageWidth: '500'
   },{
-    name: Baranaby,
-    favouriteFood: Tuna,
-    image: "http://65.media.tumblr.com/8a827e13ebb5db7d16e2b1c4cbe7ce70/tumblr_odtp4ftIhB1uhevdso1_400.jpg",
-    imageSize: '500'
+    name: 'Baranaby',
+    favouriteFood: 'Tuna',
+    image: "http://static.tumblr.com/4d57a7a32617d66936094dd0c858258b/txrsv9l/QkWn1eqmy/tumblr_static_cute-cat-image.jpg",
+    imageWidth: '500'
   },{
     name: 'Max', 
     favouriteFood: 'Whiskas',
     image: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg",
-    imageSize: '500'
+    imageWidth: '500'
   }
 ]
 
-
-var createList = function(array){
+var createList = function(){
   var list = document.createElement('ul');
-  for(var i = 0; i < array.length; i++) {
-          var item = document.createElement('li');
-          item.appendChild(document.createTextNode(array[i]));
-          list.appendChild(item);
-      }
+  list.classList.add('cat');
+  return list;
+}
+
+var createListItem= function(text){
+  var li = document.createElement('li');
+    li.innerText = text;
+    return li;
+}
+
+var createImage = function(imageUrl , imageWidth){
+  var imageLi = document.createElement('li');
+  var image = document.createElement('img');
+  image.src = imageUrl;
+  image.width = imageWidth;
+  imageLi.appendChild(image);
+  return imageLi;
+}
+
+var appendElements = function(list, name, favouriteFood,image){
+  list.appendChild(name);
+  list.appendChild(favouriteFood);
+  list.appendChild(image);
+  var catsSection = document.getElementById('cats');
+  catsSection.appendChild(list);
+}
+
+var addCat = function(name, favouriteFood, image, imageWidth ){
+  var list = createList();
+  var name = createListItem(name);
+  var favouriteFood = createListItem(favouriteFood);
+  var image = createImage(image, imageWidth);
+  appendElements(list, name, favouriteFood, image);
 }
 
 
-
 var app = function(){
-
-
-
+  cats.forEach(function(cat){
+    addCat(cat.name, cat.favouriteFood, cat.image ,cat.imageWidth)
+  })
 }
 
 
